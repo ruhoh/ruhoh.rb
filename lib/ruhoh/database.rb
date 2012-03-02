963e8bf91a35d94421303af66736346a718ca38c
Jade Dominguez <plusjade@gmail.com>
2012-03-02 13:49:31 -0800

    Refactor Database and make Observable

--- lib/ruhoh/database.rb
+++ lib/ruhoh/database.rb
@@ -1,46 +1,57 @@
+require "observer"
+
 class Ruhoh
   
   class Database
+    
     class << self
-      attr_accessor :config, :routes, :posts, :pages, :layouts, :partials
-    end
-    
-    def self.get(name)
-      self.__send__ name.to_s
-    end
+      include Observable
+      
+      # Note this is class-level so you have to call it manually.
+      def initialize
+        @config       =  ''
+        @routes       =  ''
+        @posts        =  ''
+        @pages        =  ''
+        @layouts      =  ''
+        @partials     =  ''
+        self.update!
+      end
+      
+      def get(name)
+        self.instance_variable_get("@#{name}")
+      end
 
-    def self.update
-      self.update_config
-      self.update_routes
-      self.update_posts
-      self.update_pages
-      self.update_layouts
-      self.update_partials
-    end
-    
-    def self.update_config
-      @config = YAML.load_file( File.join(Ruhoh.paths.site_source, '_config.yml') )
-    end
-    
-    def self.update_routes
-      @routes = Ruhoh::Routes.generate
-    end
-    
-    def self.update_posts
-      @posts = Ruhoh::Posts.generate
-    end
-    
-    def self.update_pages
-      @pages = Ruhoh::Pages.generate
-    end
+      def update(name)
+        self.instance_variable_set("@#{name}", 
+          case name
+          when :config
+            YAML.load_file( File.join(Ruhoh.paths.site_source, '_config.yml') )
+          when :routes
+            Ruhoh::Routes.generate
+          when :posts
+            Ruhoh::Posts.generate
+          when :pages
+            Ruhoh::Pages.generate
+          when :layouts
+            Ruhoh::Layouts.generate
+          when :partials
+            Ruhoh::Partials.generate
+          else
+            raise "Data type: '#{name}' is not a valid data type."
+          end
+        )
+        changed
+        notify_observers(name)
+      end
 
-    def self.update_layouts
-      @layouts = Ruhoh::Layouts.generate
-    end
-    
-    def self.update_partials
-      @partials = Ruhoh::Partials.generate
-    end
+      def update!
+        self.instance_variables.each { |var|
+          self.__send__ :update, var.to_s.gsub('@', '').to_sym
+        }
+      end
+      
+    end #self
     
   end #Database
   
