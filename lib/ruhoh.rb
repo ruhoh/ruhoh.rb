7b7a261182f249767f567d35e06dbe7fd71232fe
Jade Dominguez <plusjade@gmail.com>
2012-03-01 17:28:06 -0800

    Page class to own file

--- lib/ruhoh.rb
+++ lib/ruhoh.rb
@@ -14,6 +14,7 @@ require 'ruhoh/parsers/layouts'
 require 'ruhoh/parsers/partials'
 require 'ruhoh/database'
 require 'ruhoh/helper_mustache'
+require 'ruhoh/page'
 require 'ruhoh/watch'
 
 class Ruhoh
@@ -53,47 +54,6 @@ class Ruhoh
     c.theme_path = File.join('_themes', c.theme)
     
     self.config = c
-  end
-  
-  class Page
-    attr_accessor :data, :content, :sub_layout, :master_layout
-
-    def update(url)
-      self.find(url)
-      self.process_layouts
-    end
-    
-    def find(url)
-      url = '/index.html' if url == '/'
-      id = Ruhoh::Database.get(:routes)[url]
-      raise "Page id not found for url: #{url}" unless id
-      
-      @data = id =~ /^_posts/ ? Ruhoh::Database.get(:posts)['dictionary'][id] : Ruhoh::Database.get(:pages)[id]
-      raise "Page #{id} not found in database" unless @data
-
-      @content = Ruhoh::Utils.parse_file(id)['content']
-    end
-    
-    # Layouts
-    def process_layouts
-      @sub_layout = Ruhoh::Database.get(:layouts)[@data['layout']]
-      
-      if @sub_layout['data']['layout']
-        @master_layout = Ruhoh::Database.get(:layouts)[@sub_layout['data']['layout']]
-      end
-    end
-    
-    def render
-      Ruhoh::Template.process(self)
-    end
-    
-    def attributes
-      {
-        "data" => @data,
-        "content" => @content
-      }
-    end
-    
   end
   
   module Template
---
+++ lib/ruhoh/page.rb
@@ -0,0 +1,44 @@
+class Ruhoh
+
+  class Page
+    attr_accessor :data, :content, :sub_layout, :master_layout
+
+    def update(url)
+      self.find(url)
+      self.process_layouts
+    end
+    
+    def find(url)
+      url = '/index.html' if url == '/'
+      id = Ruhoh::Database.get(:routes)[url]
+      raise "Page id not found for url: #{url}" unless id
+      
+      @data = id =~ /^_posts/ ? Ruhoh::Database.get(:posts)['dictionary'][id] : Ruhoh::Database.get(:pages)[id]
+      raise "Page #{id} not found in database" unless @data
+
+      @content = Ruhoh::Utils.parse_file(id)['content']
+    end
+    
+    # Layouts
+    def process_layouts
+      @sub_layout = Ruhoh::Database.get(:layouts)[@data['layout']]
+      
+      if @sub_layout['data']['layout']
+        @master_layout = Ruhoh::Database.get(:layouts)[@sub_layout['data']['layout']]
+      end
+    end
+    
+    def render
+      Ruhoh::Template.process(self)
+    end
+    
+    def attributes
+      {
+        "data" => @data,
+        "content" => @content
+      }
+    end
+    
+  end #Page
+  
+end #Ruhoh
