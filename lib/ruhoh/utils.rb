d8505ecab4af6d4b2e75b4d329cff283563cc013
Jade Dominguez <plusjade@gmail.com>
2012-03-01 16:19:40 -0800

    Add ability to pass variable args to .parse_file and return hash
    Makes it easier to pass relative paths without having to do file.join all the time

--- lib/ruhoh/utils.rb
+++ lib/ruhoh/utils.rb
@@ -5,15 +5,22 @@ class Ruhoh
     FMregex = /^---\n(.|\n)*---\n/
     ContentRegex = /\{\{\s*content\s*\}\}/i
     
-    def self.parse_file(file_path)
-      page = File.open(file_path).read
+    # Relative file_path from site_source
+    def self.parse_file(*args)
+      path = File.__send__ :join, args.unshift(Ruhoh.config.site_source_path)
+
+      raise "File not found: #{path}" unless File.exist?(path)
+
+      page = File.open(path).read
       front_matter = page.match(FMregex)
       raise "Invalid Frontmatter" unless front_matter
 
       data = YAML.load(front_matter[0].gsub(/---\n/, "")) || {}
-      content = page.gsub(FMregex, '')
-    
-      [data, content]
+
+      { 
+        "data" => data, 
+        "content" => page.gsub(FMregex, '')
+      }
     end
   
   end
