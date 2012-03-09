44c7547909a51e030946e517c2ff47a5d55e47e8
Jade Dominguez <plusjade@gmail.com>
2012-03-08 16:21:51 -0800

    Add spec for site parser

---
+++ spec/parsers/site_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+module Site
+  
+  describe Ruhoh::Parsers::Site do
+    
+    describe "#generate" do
+
+      before(:each) do
+        Ruhoh::Utils.should_receive(:parse_file_as_yaml).and_return({'theme' => "twitter"})
+        Ruhoh.setup(SampleSitePath)
+      end
+      
+      it 'should parse the config and site yaml files' do
+        Ruhoh::Utils.should_receive(:parse_file_as_yaml).with(Ruhoh.paths.site_source, Ruhoh.files.site).and_return({})
+        Ruhoh::Utils.should_receive(:parse_file_as_yaml).with(Ruhoh.paths.site_source, Ruhoh.files.config).and_return({})
+
+        Ruhoh::Parsers::Site.generate
+      end
+      
+      it 'should return a site hash with config set as value to key "config" ' do
+        Ruhoh::Utils.should_receive(:parse_file_as_yaml).with(Ruhoh.paths.site_source, Ruhoh.files.site).and_return({"nav" => [1,2,3]})
+        Ruhoh::Utils.should_receive(:parse_file_as_yaml).with(Ruhoh.paths.site_source, Ruhoh.files.config).and_return({"theme" => "orange"})
+        
+        site = Ruhoh::Parsers::Site.generate
+        site.should == { "nav" => [1,2,3], "config" => {"theme" => "orange"} }
+      end
+      
+    end
+  end
+  
+end
