681492ed79936b1212852bbf33e006f9463d450c
Jade Dominguez <plusjade@gmail.com>
2012-03-09 17:09:16 -0800

    Refactor DB
    No longer need initialize, just update!. Whitelist the expected variables. Turns out Observer class and rspec test add class level instance variables so better to whitelist"

--- lib/ruhoh/db.rb
+++ lib/ruhoh/db.rb
@@ -3,24 +3,12 @@ require "observer"
 class Ruhoh
   
   # Public: Database class for interacting with "data" in Ruhoh.
-  #
   class DB
-    
     class << self
       include Observable
-      attr_reader :site, :routes, :posts, :pages, :layouts, :partials
+      WhiteList = [:site, :routes, :posts, :pages, :layouts, :partials]
+      self.__send__ :attr_reader, *WhiteList
 
-      # Note this is class-level so you have to call it manually.
-      def initialize
-        @site       =  ''
-        @routes       =  ''
-        @posts        =  ''
-        @pages        =  ''
-        @layouts      =  ''
-        @partials     =  ''
-        self.update!
-      end
-      
       def update(name)
         self.instance_variable_set("@#{name}", 
           case name
@@ -45,9 +33,9 @@ class Ruhoh
       end
 
       def update!
-        self.instance_variables.each { |var|
-          self.__send__ :update, var.to_s.gsub('@', '').to_sym
-        }
+        WhiteList.each do |var|
+          self.__send__ :update, var
+        end
       end
       
     end #self
