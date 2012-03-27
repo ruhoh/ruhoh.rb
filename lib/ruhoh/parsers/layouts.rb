3f2bd12a69c38c112ff9a4811a9c8206b9c0c339
Jade Dominguez <plusjade@gmail.com>
2012-03-26 23:44:08 -0700

    Add self.files for retrieving layouts

--- lib/ruhoh/parsers/layouts.rb
+++ lib/ruhoh/parsers/layouts.rb
@@ -1,26 +1,30 @@
 class Ruhoh
-  
   module Parsers
-    
     module Layouts
 
       # Generate layouts only from the active theme.
       def self.generate
         layouts = {}
-        FileUtils.cd(Ruhoh.paths.layouts) {
-          Dir.glob("**/*.*") { |filename|
-            next if FileTest.directory?(filename)
-            next if ['_','.'].include? filename[0]
-            id = File.basename(filename, File.extname(filename))
-            layouts[id] = Ruhoh::Utils.parse_file(Ruhoh.paths.layouts, filename)
-            raise "Invalid Frontmatter in layout: #{filename}" if layouts[id].empty?
-          }
-        }
+        
+        self.files.each do |filename|
+          id = File.basename(filename, File.extname(filename))
+          layouts[id] = Ruhoh::Utils.parse_file(Ruhoh.paths.layouts, filename)
+          raise "Invalid Frontmatter in layout: #{filename}" if layouts[id].empty?
+        end
+        
         layouts
       end
 
+      def self.files
+        FileUtils.cd(Ruhoh.paths.layouts) {
+          return Dir["**/*.*"].select { |filename|
+            next if FileTest.directory?(filename)
+            next if ['_','.'].include? filename[0]
+            true
+          }
+        }
+      end
+      
     end #Layouts
-  
   end #Parsers
-  
 end #Ruhoh
