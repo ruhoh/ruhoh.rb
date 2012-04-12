d2736a80338795298fe17fe0ccefab340e3b0c85
Jade Dominguez <plusjade@gmail.com>
2012-04-11 17:30:37 -0700

    Add logger class
    The logger should handle log warnings and errors and handle stdout messages. This is a super simple implementation influenced by DataMapper.

---
+++ lib/ruhoh/logger.rb
@@ -0,0 +1,37 @@
+class Ruhoh
+  class Logger
+    
+    attr_reader :buffer
+    attr_accessor :log_file
+
+    def initialize
+      @buffer = []
+    end
+    
+    def error(string=nil)
+      message = ""
+      message << string if string
+      message << "\n" unless message[-1] == ?\n
+      @buffer << message
+      
+      self.on_error
+    end  
+    
+    def on_error
+      msg = @buffer[0]
+      Ruhoh::Friend.say { red msg }
+      self.to_file
+      exit -1
+    end
+    
+    def to_file
+      return unless self.log_file && @buffer.size > 0
+      File.open(self.log_file, "a") { |f|
+        f.puts '---'
+        f.puts Time.now.utc
+        f.puts @buffer.slice!(0..-1).join
+      }
+    end
+
+  end #Logger
+end #Ruhoh
