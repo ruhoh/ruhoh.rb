fe203a3a10d41edc29a53d351c0cf986efee7a60
Ben Biddington <ben.biddington@gmail.com>
2012-04-29 14:27:44 +1200

    [GREEN] Fixed the layout test to create the files it expects to exist.

--- spec/parsers/layouts_spec.rb
+++ spec/parsers/layouts_spec.rb
@@ -5,9 +5,30 @@ module Layouts
   describe Ruhoh::Parsers::Layouts do
     
     before(:each) do
-      Ruhoh::Utils.should_receive(:parse_file_as_yaml).and_return({'theme' => "twitter"})
+      expected_theme = "twitter"
+      
+      Ruhoh::Utils.should_receive(:parse_file_as_yaml).and_return({'theme' => expected_theme})
       Ruhoh.setup(:source => SampleSitePath)
+
+      the_layouts_dir = File.join SampleSitePath, "_templates", "themes", expected_theme, "layouts"
+
+      FileUtils.remove_dir(the_layouts_dir, 1) if Dir.exists? the_layouts_dir
+      FileUtils.makedirs the_layouts_dir
+
+      expected_layouts.each do |layout_name| 
+        full_file_name = File.join(the_layouts_dir, layout_name)
+        
+        File.open full_file_name, "w+" do |file|
+          file.puts <<-TEXT
+---
+title: #{layout_name} (test)
+---  
+          TEXT
+        end
+      end
     end
+
+    let(:expected_layouts) { %w{default.html page.html post.html} }
     
     describe "#generate" do
       let(:layouts){
