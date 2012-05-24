cd6083ab780cd788afe60be16736bdaf9a9b5558
Jade Dominguez <plusjade@gmail.com>
2012-05-24 00:32:06 -0700

    Add hash deep_merge method

--- lib/ruhoh/utils.rb
+++ lib/ruhoh/utils.rb
@@ -61,6 +61,25 @@ class Ruhoh
       end
     end
     
+    # Merges hash with another hash, recursively.
+    #
+    # Adapted from Jekyll which got it from some gem whose link is now broken.
+    # Thanks to whoever made it.
+    def self.deep_merge(hash1, hash2)
+      target = hash1.dup
+
+      hash2.keys.each do |key|
+        if hash2[key].is_a? Hash and hash1[key].is_a? Hash
+          target[key] = self.deep_merge(target[key], hash2[key])
+          next
+        end
+
+        target[key] = hash2[key]
+      end
+
+      target
+    end
+    
   end
   
 end #Ruhoh
