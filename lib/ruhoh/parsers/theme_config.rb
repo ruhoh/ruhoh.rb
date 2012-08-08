a258dc001137c589d9b4e52299969a88e2914ceb
Jade Dominguez <plusjade@gmail.com>
2012-08-08 02:07:22 -0700

    Parse new 'exclude' theme_config parameter which allows themes to specify filepaths to exclude from copying over to compiled site.
    This allows us to switch to a 'copy everything' strategy rather then the whitelist strategy from before, which caused too many problems of not loading needed assets.

--- lib/ruhoh/parsers/theme_config.rb
+++ lib/ruhoh/parsers/theme_config.rb
@@ -13,6 +13,15 @@ class Ruhoh
         end
         return {} unless config.is_a? Hash
         
+        config["exclude"] = Array(config['exclude']).compact.map do |node| 
+          is_last = node[0] == "*"
+          node = node.chomp("*").reverse.chomp("*").reverse
+          node = Regexp.escape("#{node}")
+          node = is_last ? "#{node}$" : "^#{node}"
+          
+          Regexp.new(node, true)
+        end
+        
         config
       end
       
