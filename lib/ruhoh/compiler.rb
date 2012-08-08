ddb9ba34fb9d1ea177f48f03c9421227bd39a587
Jade Dominguez <plusjade@gmail.com>
2012-08-08 02:11:09 -0700

    Javascripts still need to be copied via white-list since widgets may register javascripts.

--- lib/ruhoh/compiler.rb
+++ lib/ruhoh/compiler.rb
@@ -26,6 +26,7 @@ class Ruhoh
       def self.run(target, page)
         self.pages(target, page)
         self.media(target, page)
+        self.javascripts(target, page)
       end
       
       def self.pages(target, page)
@@ -47,6 +48,22 @@ class Ruhoh
         media = Ruhoh::Utils.url_to_path(url, target)
         FileUtils.mkdir_p media
         FileUtils.cp_r File.join(Ruhoh.paths.media, '.'), media
+      end
+      
+      # Create all the javascripts.
+      # Javascripts may be registered from either a theme or a widget.
+      # Technically the theme compiler may create javascripts relative to the theme.
+      # This ensures the widget javascripts are created as well.
+      def self.javascripts(target, page)
+        Ruhoh::DB.javascripts.each do |type, assets|
+          assets.each do |asset|
+            url = asset['url'].gsub(/^\//, '')
+            next unless File.exist?(asset['id'])
+            file_path = Ruhoh::Utils.url_to_path(File.dirname(url), target)
+            FileUtils.mkdir_p file_path
+            FileUtils.cp(asset['id'], file_path)
+          end
+        end
       end
       
     end #Defaults
