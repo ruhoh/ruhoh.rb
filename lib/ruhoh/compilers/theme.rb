497b60ac4094e675c1182733fa81245f997606ab
Jade Dominguez <plusjade@gmail.com>
2012-08-08 02:10:08 -0700

    Theme compiler now copies _everything_ over instead of white-listed folders.
    This gives theme designers much more freedom. Themes may also specify paths to folders/files to exclude via theme.yml

--- lib/ruhoh/compilers/theme.rb
+++ lib/ruhoh/compilers/theme.rb
@@ -3,42 +3,44 @@ class Ruhoh
     module Theme
       
       def self.run(target, page)
-        self.stylesheets(target, page)
-        self.javascripts(target, page)
-        self.media(target, page)
-      end
-      
-      def self.stylesheets(target, page)
-        Ruhoh::DB.stylesheets.each do |type, assets|
-          assets.each do |asset|
-            url = asset['url'].gsub(/^\//, '')
-            next unless File.exist?(asset['id'])
-            file_path = Ruhoh::Utils.url_to_path(File.dirname(url), target)
-            FileUtils.mkdir_p file_path
-            FileUtils.cp(asset['id'], file_path)
-          end
-        end
+        self.copy(target, page)
       end
 
-      def self.javascripts(target, page)
-        Ruhoh::DB.javascripts.each do |type, assets|
-          assets.each do |asset|
-            url = asset['url'].gsub(/^\//, '')
-            next unless File.exist?(asset['id'])
-            file_path = Ruhoh::Utils.url_to_path(File.dirname(url), target)
-            FileUtils.mkdir_p file_path
-            FileUtils.cp(asset['id'], file_path)
-          end
+      # Copies all theme assets over to the compiled site.
+      # Note the compiled assets are namespaced at /assets/<theme-name>/
+      # theme.yml may specify exclusion rules for excluding assets.
+      def self.copy(target, page)
+        url = Ruhoh.urls.theme.gsub(/^\//, '')
+        theme = Ruhoh::Utils.url_to_path(url, target)
+        FileUtils.mkdir_p theme
+
+        self.files.each do |file|
+          original_file = File.join(Ruhoh.paths.theme, file)
+          compiled_file = File.join(theme, file)
+          FileUtils.mkdir_p File.dirname(compiled_file)
+          FileUtils.cp_r original_file, compiled_file
         end
       end
       
-      def self.media(target, page)
-        return unless FileTest.directory? Ruhoh.paths.theme_media
-        url = Ruhoh.urls.theme_media.gsub(/^\//, '')
-        theme_media = Ruhoh::Utils.url_to_path(url, target)
-        FileUtils.mkdir_p theme_media
-        FileUtils.cp_r File.join(Ruhoh.paths.theme_media, '.'), theme_media
+      # Returns list of all files from the theme that need to be
+      # compiled to the production environment.
+      # Returns Array of relative filepaths
+      def self.files
+        FileUtils.cd(Ruhoh.paths.theme) {
+          return Dir["**/*"].select { |filepath|
+            next unless self.is_valid_asset?(filepath)
+            true
+          }
+        }
       end
+
+      # Checks a given asset filepath against any user-defined exclusion rules in theme.yml
+      def self.is_valid_asset?(filepath)
+        return false if FileTest.directory?(filepath)
+        Ruhoh::DB.theme_config["exclude"].each {|regex| return false if filepath =~ regex }
+        true
+      end
+      
     end #Theme
   end #Compiler
 end #Ruhoh
