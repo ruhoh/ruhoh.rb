be32252033c8c6798799ecaa4000cd178f328d03
Jade Dominguez <plusjade@gmail.com>
2012-10-09 21:31:55 -0700

    Remove config from site hash
    config can be access directly using the database

--- lib/ruhoh/parsers/site.rb
+++ lib/ruhoh/parsers/site.rb
@@ -1,13 +1,10 @@
 class Ruhoh
   module Parsers
-    # Sitewide data hash + configuration file.
+
     class Site < Base
 
       def generate
-        site = Ruhoh::Utils.parse_yaml_file(@ruhoh.paths.site_data) || {}
-        config = Ruhoh::Utils.parse_yaml_file(@ruhoh.paths.config_data)
-        site['config'] = config
-        site
+        Ruhoh::Utils.parse_yaml_file(@ruhoh.paths.site_data) || {}
       end
 
     end
