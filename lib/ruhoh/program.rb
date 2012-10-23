d553b18403d06d5594bbb4a937220985540a470f
Jade Dominguez <plusjade@gmail.com>
2012-10-22 21:08:40 -0700

    Inline the page previewer

--- lib/ruhoh/program.rb
+++ lib/ruhoh/program.rb
@@ -1,5 +1,3 @@
-require 'ruhoh/previewers/page'
-
 class Ruhoh
   module Program
     
@@ -49,9 +47,34 @@ class Ruhoh
         end
         
         map '/' do
-          run Ruhoh::Previewer::Page.new(ruhoh)
+          run PagePreviewer.new(ruhoh)
         end
       }
+    end
+    
+    # Public: Rack application used to render singular pages via their URL.
+    class PagePreviewer
+    
+      def initialize(ruhoh)
+        @ruhoh = ruhoh
+      end
+    
+      def call(env)
+        return favicon if env['PATH_INFO'] == '/favicon.ico'
+
+        # Always remove trailing slash if sent unless it's the root page.
+        env['PATH_INFO'].gsub!(/\/$/, '') unless env['PATH_INFO'] == "/"
+        
+        pointer =  @ruhoh.db.routes[env['PATH_INFO']]
+        raise "Page id not found for url: #{env['PATH_INFO']}" unless pointer
+        page = @ruhoh.page(pointer)
+        [200, {'Content-Type' => 'text/html'}, [page.render]]
+      end
+    
+      def favicon
+        [200, {'Content-Type' => 'image/x-icon'}, ['']]
+      end
+
     end
     
     # Public: A program for compiling to a static website.
