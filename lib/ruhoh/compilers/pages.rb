9bed26125c661a04571b3b38e591fd4b3eae1c6b
Jade Dominguez <plusjade@gmail.com>
2012-10-22 20:51:15 -0700

    Fix score compilers

--- lib/ruhoh/compilers/pages.rb
+++ lib/ruhoh/compilers/pages.rb
@@ -8,13 +8,14 @@ class Ruhoh
       
       def run
         FileUtils.cd(@ruhoh.paths.compiled) {
-          @ruhoh.db.all_pages.each_value do |p|
-            page = @ruhoh.page(p['id'])
+          @ruhoh.db.pages.each_value do |data|
+            
+            page = @ruhoh.page(data['pointer'])
 
             FileUtils.mkdir_p File.dirname(page.compiled_path)
             File.open(page.compiled_path, 'w:UTF-8') { |p| p.puts page.render }
 
-            Ruhoh::Friend.say { green "processed: #{p['id']}" }
+            Ruhoh::Friend.say { green "processed: #{data['id']}" }
           end
         }
       end
--- lib/ruhoh/compilers/rss.rb
+++ lib/ruhoh/compilers/rss.rb
@@ -12,11 +12,8 @@ class Ruhoh
         @ruhoh = ruhoh
       end
       
-      # TODO: This renders the page content even though we already need to
-      # render the content to save to disk. This will be a problem when
-      # posts numbers expand. Merge this in later.
       def run
-        num_posts = @ruhoh.config.rss_limit
+        num_posts = @ruhoh.db.config("posts")["rss_limit"]
         posts = @ruhoh.db.posts.each_value.map { |val| val }
         posts.sort! {
           |a,b| Date.parse(b['date']) <=> Date.parse(a['date'])
@@ -27,16 +24,15 @@ class Ruhoh
          xml.rss(:version => '2.0') {
            xml.channel {
              xml.title_ @ruhoh.db.site['title']
-             xml.link_ @ruhoh.db.site['config']['production_url']
+             xml.link_ @ruhoh.config['production_url']
              xml.pubDate_ Time.now          
-             posts.each do |post_id|
-               post = @ruhoh.db.posts[post_id]
-               page = @ruhoh.page(post_id)
+             posts.each do |data|
+               page = @ruhoh.page(data["pointer"])
                xml.item {
-                 xml.title_ post['title']
-                 xml.link "#{@ruhoh.db.site['config']['production_url']}#{post['url']}"
-                 xml.pubDate_ post['date']
-                 xml.description_ (post['description'] ? post['description'] : page.render_content)
+                 xml.title_ data['title']
+                 xml.link "#{@ruhoh.config['production_url']}#{data['url']}"
+                 xml.pubDate_ data['date']
+                 xml.description_ (data['description'] ? data['description'] : page.render_content)
                }
              end
            }
