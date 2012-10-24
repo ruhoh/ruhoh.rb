93b2480f7f9348ef261723f5919f5865a7873997
Jade Dominguez <plusjade@gmail.com>
2012-10-23 21:43:51 -0700

    Plugins define their own command line client methods

---
+++ lib/ruhoh/client.rb
@@ -0,0 +1,158 @@
+require 'ruhoh/compiler'
+require 'ruhoh/console_methods'
+require 'irb'
+
+class Ruhoh
+  
+  class Client
+    DefaultBlogScaffold = 'git://github.com/ruhoh/blog.git'
+    Help = [
+      {
+        "command" => "new <directory_path>",
+        "desc" => "Create a new blog directory based on the Ruhoh specification."
+      },
+      {
+        "command" => "compile",
+        "desc" => "Compile to static website."
+      },
+      {
+        "command" => "help",
+        "desc" => "Show this menu."
+      }
+    ]
+    def initialize(data)
+      @args = data[:args]
+      @options = data[:options]
+      @opt_parser = data[:opt_parser]
+      @options.ext = (@options.ext || 'md').gsub('.', '')
+      
+      cmd = (data[:args][0] == 'new') ? 'blog' : (data[:args][0] || 'help')
+      
+      return self.__send__(cmd) if self.respond_to?(cmd)
+
+      Ruhoh::Friend.say { 
+        red "Plugin #{cmd} not found"
+        exit 
+      } unless Ruhoh::Plugins::Plugin.plugins.has_key?(cmd)
+      
+      @ruhoh = Ruhoh.new
+      @ruhoh.setup
+      @ruhoh.setup_paths
+      
+      klass = Ruhoh::Plugins::Plugin.plugins[cmd].const_get(:Client)
+      client = klass.new(@ruhoh, data)
+      
+      Ruhoh::Friend.say { 
+        red "method '#{data[:args][1]}' not found for #{klass}"
+        exit 
+      } unless client.respond_to?(@args[1])
+      
+      client.__send__(@args[1])
+    end  
+    
+    # Thanks rails! https://github.com/rails/rails/blob/master/railties/lib/rails/commands/console.rb
+    def console
+      ARGV.clear # IRB throws an error otherwise.
+      require 'pp'
+      IRB::ExtendCommandBundle.send :include, Ruhoh::ConsoleMethods
+      IRB.start
+    end
+    
+    # Show Client Utility help documentation.
+    def help
+      options = @opt_parser.help
+      plugins = [{"methods" => Help}]
+      plugins += Ruhoh::Plugins::Plugin.plugins.each.map {|name, klass|
+        next unless klass.const_defined?(:Client)
+        next unless klass.const_get(:Client).const_defined?(:Help)
+        {
+          "name" => name,
+          "methods" => klass.const_get(:Client).const_get(:Help)
+        }
+      }.compact
+      
+      Ruhoh::Friend.say { 
+        plain "Ruhoh is a nifty, modular static blog generator."
+        plain "It is the Universal Static Blog API."
+        plain "Visit http://www.ruhoh.com for complete usage and documentation."
+        plain ''
+        plain options
+        plain ''
+        plain 'Commands:'
+        plain ''
+        plugins.each do |plugin|
+          plugin["methods"].each do |method|
+            if plugin["name"]
+              green("  " + "#{plugin["name"]} #{method["command"]}")
+            else
+              green("  " + method["command"])
+            end
+            plain("    "+ method["desc"])
+          end
+        end
+      }
+    end
+    
+    # Public: Compile to static website.
+    def compile
+      Ruhoh::Program.compile(@args[1])
+    end
+    
+    # Public: Create a new blog at the directory provided.
+    def blog
+      name = @args[1]
+      scaffold = @args.length > 2 ? @args[2] : DefaultBlogScaffold
+      useHg = @options.hg
+      Ruhoh::Friend.say { 
+        red "Please specify a directory path." 
+        plain "  ex: ruhoh new the-blogist"
+        exit
+      } if name.nil?
+
+      target_directory = File.join(Dir.pwd, name)
+
+      Ruhoh::Friend.say { 
+        red "#{target_directory} already exists."
+        plain "  Specify another directory or `rm -rf` this directory first."
+        exit
+      } if File.exist?(target_directory)
+      
+      Ruhoh::Friend.say { 
+        plain "Trying this command:"
+
+        if useHg
+          cyan "  hg clone #{scaffold} #{target_directory}"
+          success = system('hg', 'clone', scaffold, target_directory)
+        else
+          cyan "  git clone #{scaffold} #{target_directory}"
+          success = system('git', 'clone', scaffold, target_directory)
+        end
+
+        if success
+          green "Success! Now do..."
+          cyan "  cd #{target_directory}"
+          cyan "  rackup -p9292"
+          cyan "  http://localhost:9292"
+        else
+          red "Could not git clone blog scaffold. Please try it manually:"
+          cyan "  git clone git://github.com/ruhoh/blog.git #{target_directory}"
+        end
+      }
+    end
+    
+    def ask(message, valid_options)
+      if valid_options
+        answer = get_stdin("#{message} #{valid_options.to_s.gsub(/"/, '').gsub(/, /,'/')} ") while !valid_options.include?(answer)
+      else
+        answer = get_stdin(message)
+      end
+      answer
+    end
+
+    def get_stdin(message)
+      print message
+      STDIN.gets.chomp
+    end
+    
+  end #Client
+end #Ruhoh
--- lib/ruhoh/plugins/layouts.rb
+++ lib/ruhoh/plugins/layouts.rb
@@ -12,5 +12,51 @@ module Ruhoh::Plugins
       end
     end
 
+    class Client
+      Help = [
+        {
+          "command" => "new <name>",
+          "desc" => "Create a new layout for the currently active theme."
+        }
+      ]      
+
+      def initialize(ruhoh, data)
+        @ruhoh = ruhoh
+        @args = data[:args]
+        @options = data[:options]
+        @opt_parser = data[:opt_parser]
+        @options.ext = (@options.ext || 'md').gsub('.', '')
+      end
+      
+      # Public: Create a new layout file for the active theme.
+      def new
+        ruhoh = @ruhoh
+        name = @args[1]
+        Ruhoh::Friend.say { 
+          red "Please specify a layout name." 
+          cyan "ex: ruhoh new layout splash"
+          exit
+        } if name.nil?
+
+        filename = File.join(@ruhoh.paths.theme_layouts, name.gsub(/\s/, '-').downcase) + ".html"
+        if File.exist?(filename)
+          abort("Create new layout: aborted!") if ask("#{filename} already exists. Do you want to overwrite?", ['y', 'n']) == 'n'
+        end
+
+        @ruhoh.db.update(:scaffolds)
+
+        FileUtils.mkdir_p File.dirname(filename)
+        File.open(filename, 'w:UTF-8') do |page|
+          page.puts @ruhoh.db.scaffolds['layout.html'].to_s
+        end
+
+        Ruhoh::Friend.say {
+          green "New layout:"
+          plain ruhoh.relative_path(filename)
+        }
+      end
+      
+    end
+    
   end
 end
