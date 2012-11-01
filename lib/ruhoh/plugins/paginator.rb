710e95e1a77334b028551528303a138ccb2d49d0
Jade Dominguez <plusjade@gmail.com>
2012-10-31 20:48:14 -0700

    A working (incomplete) example of paginator plugin

---
+++ lib/ruhoh/plugins/paginator.rb
@@ -0,0 +1,43 @@
+module Ruhoh::Plugins
+  class Paginator < Plugin
+    
+    def config
+      hash = super
+      hash["namepace"] ||=  "/index"
+      hash["namepace"] = hash["namepace"].to_s
+      unless hash["namepace"][0] == "/"
+        hash["namepace"] = "/#{hash["namepace"]}"
+      end
+      hash["per_page"] ||=  5
+      hash["per_page"] = hash["per_page"].to_i
+      hash["layout"] ||=  "paginator"
+      
+      hash["root_page"] ||=  "/index"
+      hash
+    end
+
+    def generate
+    end
+    
+    def url_endpoint
+      config["namespace"]
+    end
+    
+    class Previewer
+      def initialize(plugin)
+        @plugin = plugin
+        @ruhoh = plugin.ruhoh
+      end
+
+      def call(env)
+        # Always remove trailing slash if sent unless it's the root page.
+        env['PATH_INFO'].gsub!(/\/$/, '') unless env['PATH_INFO'] == "/"
+        path = env['PATH_INFO'].reverse.chomp("/").reverse
+        page = @ruhoh.page("nothing")
+        page.data = {"layout" => @ruhoh.db.config("paginator")["layout"], "current_page" => path}
+        [200, {'Content-Type' => 'text/html'}, [page.render]]
+      end
+    end
+    
+  end
+end  
--- lib/ruhoh/templaters/posts_helpers.rb
+++ lib/ruhoh/templaters/posts_helpers.rb
@@ -1,7 +1,38 @@
 class Ruhoh
   module Templaters
     module PostsHelpers
+      
+      # current_page is set via a compiler or previewer
+      # in which it can discern what current_page to serve
+      def posts_paginator
+        per_page = @ruhoh.db.config("paginator")["per_page"]
+        current_page = self.context["page"]['current_page'].to_i rescue 0
+        current_page = current_page.zero? ? 1 : current_page
+        offset = (current_page-1)*per_page
 
+        post_batch = posts[offset, per_page]
+        raise "Page does not exist" unless post_batch
+        post_batch
+      end
+      
+      def paginator_navigation
+        config = @ruhoh.db.config("paginator")
+        post_count = @ruhoh.db.posts.length
+        total_pages = (post_count.to_f/config["per_page"]).ceil
+        current_page = self.context["page"]['current_page'].to_i rescue 0
+        current_page = current_page.zero? ? 1 : current_page
+
+        total_pages.times.map { |i| 
+          {
+            "url" => (
+              @ruhoh.to_url(i.zero? ? config["root_page"] : "#{config["namespace"]}#{i+1}")
+            ),
+            "name" => "#{i+1}",
+            "is_active_page" => (i+1 == current_page)
+          }
+        }
+      end
+        
       def posts
         posts = @ruhoh.db.posts.each_value.map { |val| val }
         posts.sort! {
