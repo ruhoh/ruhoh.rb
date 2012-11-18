d298a5bbaf226be564d0a1ec41f5144f6453f5dd
Jade Dominguez <plusjade@gmail.com>
2012-11-17 16:43:38 -0800

    Support for chained contextual helpers
    This is a contribution by @pluskid and closes #41 -- thank you very much!

--- lib/ruhoh/templaters/rmustache.rb
+++ lib/ruhoh/templaters/rmustache.rb
@@ -20,12 +20,16 @@ class Ruhoh
         # Overload find method to catch helper expressions
         def find(obj, key, default = nil)
           return super unless key.to_s.index('?')
-          context, helper = key.to_s.split('?')
+          keys = key.to_s.split('?')
+          context = keys[0]
+          helpers = keys[1..-1]
           context = context.empty? ? obj : super(obj, context)
-
-          self.mustache_in_stack.__send__ helper, context
+          
+          helpers.each do |helper|
+            context = self.mustache_in_stack.__send__ helper, context
+          end
+          context
         end
-
       end
     
       def context
