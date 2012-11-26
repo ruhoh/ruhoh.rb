686c46ab954861e0289f1271f43fb177f5b3a6b8
Jade Dominguez <plusjade@gmail.com>
2012-11-26 00:53:17 -0800

    Fix paginator previewer

--- lib/ruhoh/resources/paginator/previewer.rb
+++ lib/ruhoh/resources/paginator/previewer.rb
@@ -9,13 +9,10 @@ module Ruhoh::Resources::Paginator
       # Always remove trailing slash if sent unless it's the root page.
       env['PATH_INFO'].gsub!(/\/$/, '') unless env['PATH_INFO'] == "/"
       path = env['PATH_INFO'].reverse.chomp("/").reverse
-      page = @ruhoh.page("nothing")
+      page = @ruhoh.page({"resource" => "posts"})
       page.data = {
         "layout" => @ruhoh.db.config("paginator")["layout"],
-        "current_page" => path,
-        "pointer" => {
-          "resource" => "posts"
-        }
+        "current_page" => path
       }
       [200, {'Content-Type' => 'text/html'}, [page.render_full]]
     end
