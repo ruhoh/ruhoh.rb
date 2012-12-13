cfa594541e99cf1673130a949d433de601c8b6e6
Jade Dominguez <plusjade@gmail.com>
2012-12-12 21:57:08 -0800

    Remove unused compile method

--- lib/ruhoh/resources/pages/collection.rb
+++ lib/ruhoh/resources/pages/collection.rb
@@ -6,19 +6,5 @@ module Ruhoh::Resources::Pages
       hash['exclude'] = Array(hash['exclude']).map {|node| Regexp.new(node) }
       hash
     end
-    
-    def compile(id=nil)
-      datas = if id
-        [ @ruhoh.db.__send__(self.namespace)[id] ].compact
-      else
-        @ruhoh.db.__send__(self.namespace).each_value
-      end
-
-      datas.each do |data|
-        model.new(self, data["pointer"]).compile
-      end
-
-      nil
-    end
   end
 end
