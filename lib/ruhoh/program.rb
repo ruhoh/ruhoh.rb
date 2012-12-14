bc5a4018c1cce940df6b2ec9191eea232a0a8ec6
Jade Dominguez <plusjade@gmail.com>
2012-12-14 00:28:37 -0800

    Use chomp

--- lib/ruhoh/program.rb
+++ lib/ruhoh/program.rb
@@ -60,7 +60,7 @@ class Ruhoh
         return favicon if env['PATH_INFO'] == '/favicon.ico'
 
         # Always remove trailing slash if sent unless it's the root page.
-        env['PATH_INFO'].gsub!(/\/$/, '') unless env['PATH_INFO'] == "/"
+        env['PATH_INFO'].chomp!("/") unless env['PATH_INFO'] == "/"
         
         pointer =  @ruhoh.db.routes[env['PATH_INFO']]
         raise "Page id not found for url: #{env['PATH_INFO']}" unless pointer
