c17aa0556c44b34815ccdddcc50b163fd58dc910
Jade Dominguez <plusjade@gmail.com>
2012-12-15 15:06:44 -0800

    Move base Page previewer into Page resources folder.

--- lib/ruhoh/program.rb
+++ lib/ruhoh/program.rb
@@ -44,34 +44,9 @@ class Ruhoh
         end
         
         map '/' do
-          run PagePreviewer.new(ruhoh)
+          run Ruhoh::Resources::Page::Previewer.new(ruhoh)
         end
       }
-    end
-    
-    # Public: Rack application used to render singular pages via their URL.
-    class PagePreviewer
-    
-      def initialize(ruhoh)
-        @ruhoh = ruhoh
-      end
-    
-      def call(env)
-        return favicon if env['PATH_INFO'] == '/favicon.ico'
-
-        # Always remove trailing slash if sent unless it's the root page.
-        env['PATH_INFO'].chomp!("/") unless env['PATH_INFO'] == "/"
-        
-        pointer =  @ruhoh.db.routes[env['PATH_INFO']]
-        raise "Page id not found for url: #{env['PATH_INFO']}" unless pointer
-        page = @ruhoh.page(pointer)
-        [200, {'Content-Type' => 'text/html'}, [page.render_full]]
-      end
-    
-      def favicon
-        [200, {'Content-Type' => 'image/x-icon'}, ['']]
-      end
-
     end
     
     # Public: A program for compiling to a static website.
