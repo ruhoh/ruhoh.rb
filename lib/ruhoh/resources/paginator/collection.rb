5607d1491202ec4b871bf83fe3a3c663f5eb257e
Jade Dominguez <plusjade@gmail.com>
2013-01-08 20:26:13 -0800

    root_page for paginator should default to paginator namespace.
    Fixes #122

--- lib/ruhoh/resources/paginator/collection.rb
+++ lib/ruhoh/resources/paginator/collection.rb
@@ -2,16 +2,22 @@ module Ruhoh::Resources::Paginator
   class Collection < Ruhoh::Resources::Base::Collection
     def config
       hash = super
+
       hash["namespace"] ||=  "/index"
       hash["namespace"] = hash["namespace"].to_s
-      unless hash["namespace"][0] == "/"
+      unless hash["namespace"].start_with?('/')
         hash["namespace"] = "/#{hash["namespace"]}"
       end
+
       hash["per_page"] ||=  5
       hash["per_page"] = hash["per_page"].to_i
       hash["layout"] ||=  "paginator"
-      
-      hash["root_page"] ||=  "/index"
+
+      hash["root_page"] ||=  hash["namespace"]
+      unless hash["root_page"].start_with?('/')
+        hash["root_page"] = "/#{hash["root_page"]}"
+      end
+
       hash
     end
 
