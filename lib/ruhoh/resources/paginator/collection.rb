8e69a3363c9b49adb3e2ef77a99bc92169890e8e
Jade Dominguez <plusjade@gmail.com>
2013-01-08 20:23:41 -0800

    Fix mispelled 'namespace'

--- lib/ruhoh/resources/paginator/collection.rb
+++ lib/ruhoh/resources/paginator/collection.rb
@@ -2,10 +2,10 @@ module Ruhoh::Resources::Paginator
   class Collection < Ruhoh::Resources::Base::Collection
     def config
       hash = super
-      hash["namepace"] ||=  "/index"
-      hash["namepace"] = hash["namepace"].to_s
-      unless hash["namepace"][0] == "/"
-        hash["namepace"] = "/#{hash["namepace"]}"
+      hash["namespace"] ||=  "/index"
+      hash["namespace"] = hash["namespace"].to_s
+      unless hash["namespace"][0] == "/"
+        hash["namespace"] = "/#{hash["namespace"]}"
       end
       hash["per_page"] ||=  5
       hash["per_page"] = hash["per_page"].to_i
