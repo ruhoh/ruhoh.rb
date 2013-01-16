91d2d121f3046061705a9c06a0a7624343eec0fb
Jade Dominguez <plusjade@gmail.com>
2013-01-15 20:09:37 -0800

    Ensure media assets honor base_path.
    Thanks @zoidrr -- closes #127

--- lib/ruhoh/resources/media/compiler.rb
+++ lib/ruhoh/resources/media/compiler.rb
@@ -9,7 +9,7 @@ module Ruhoh::Resources::Media
       end
       
       Ruhoh::Friend.say { cyan "Media: (copying entire folder)" }
-      media = Ruhoh::Utils.url_to_path(collection.url_endpoint, @ruhoh.paths.compiled)
+      media = Ruhoh::Utils.url_to_path(@ruhoh.to_url(collection.url_endpoint), @ruhoh.paths.compiled)
       FileUtils.mkdir_p media
       FileUtils.cp_r(File.join(media_path, '.'), media)
       Ruhoh::Friend.say { green "  > done!" }
