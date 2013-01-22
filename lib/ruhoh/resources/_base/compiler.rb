ab5a8b851283f79e7a7cc56d5440496033e9b764
Jade Dominguez <plusjade@gmail.com>
2013-01-21 19:46:54 -0800

    Provide @collection to base compiler class as convenience.

--- lib/ruhoh/resources/_base/compiler.rb
+++ lib/ruhoh/resources/_base/compiler.rb
@@ -1,10 +1,12 @@
 module Ruhoh::Resources::Base
   class Compiler
+    attr_reader :collection
 
     def initialize(ruhoh)
       @ruhoh = ruhoh
+      @collection = @ruhoh.resources.load_collection(resource_name)
     end
-    
+
     def self.inherited(base)
       name = base.name.chomp("::Compiler").split("::").pop.downcase
       base.send(:define_method, "resource_name") do
--- lib/ruhoh/resources/media/compiler.rb
+++ lib/ruhoh/resources/media/compiler.rb
@@ -1,15 +1,14 @@
 module Ruhoh::Resources::Media
   class Compiler < Ruhoh::Resources::Base::Compiler
     def run
-      collection = @ruhoh.resources.load_collection('media')
-      media_path = File.join(@ruhoh.paths.base, collection.path)
+      media_path = File.join(@ruhoh.paths.base, @collection.path)
       unless FileTest.directory? media_path
         Ruhoh::Friend.say { yellow "Media: directory not found - skipping." }
         return
       end
       
       Ruhoh::Friend.say { cyan "Media: (copying entire folder)" }
-      media = Ruhoh::Utils.url_to_path(@ruhoh.to_url(collection.url_endpoint), @ruhoh.paths.compiled)
+      media = Ruhoh::Utils.url_to_path(@ruhoh.to_url(@collection.url_endpoint), @ruhoh.paths.compiled)
       FileUtils.mkdir_p media
       FileUtils.cp_r(File.join(media_path, '.'), media)
       Ruhoh::Friend.say { green "  > done!" }
