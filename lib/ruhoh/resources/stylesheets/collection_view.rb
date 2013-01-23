24a1ebd65c35bd47a183368debe24e84a4af073f
Jade Dominguez <plusjade@gmail.com>
2013-01-22 20:24:32 -0800

    Implement 'stylesheets.load' block helper as the new interface to load stylesheets.

--- lib/ruhoh/resources/stylesheets/collection_view.rb
+++ lib/ruhoh/resources/stylesheets/collection_view.rb
@@ -1,17 +1,28 @@
 module Ruhoh::Resources::Stylesheets
   class CollectionView < Ruhoh::Resources::Base::CollectionView
-    def all
-      buffer = ''
-      master_layout = master.page_data['master_layout']
-      sub_layout = master.page_data['sub_layout']
-      stylesheets = @ruhoh.db.stylesheets[master_layout] || []
-      stylesheets += @ruhoh.db.stylesheets[sub_layout] || []
-      stylesheets += @ruhoh.db.stylesheets["widgets"] || []
-      stylesheets.each do |style|
-        buffer += "<link href=\"#{style['url']}?#{rand()}\" type=\"text/css\" rel=\"stylesheet\" media=\"all\">\n"
-      end
-      
-      buffer
+    # Load Stylesheets as defined within the given sub_context
+    #
+    # Example:
+    #   {{# stylesheets.load }}
+    #     global.css
+    #     custom.css
+    #   {{/ stylesheets.load }}
+    #   (stylesheets are separated by newlines)
+    #
+    # This is a convenience method that will automatically create link tags
+    # with respect to ruhoh's internal URL generation mechanism; e.g. base_path
+    #
+    # @returns[String] HTML link tags for given stylesheets
+    def load(sub_context)
+      collection = @ruhoh.resources.load_collection("stylesheets")
+      stylesheets = sub_context.split("\n").map{ |s| s.gsub(/\s/, '') }.delete_if(&:empty?)
+      stylesheets.map { |name|
+        url = (name =~ /^(http:|https:)?\/\//i) ?
+          name :
+          (@ruhoh.to_url(collection.url_endpoint, name) + "?#{rand()}")
+        
+        "<link href='#{url}' type='text/css' rel='stylesheet' media='all'>"
+      }.join("\n")
     end
   end
 end
