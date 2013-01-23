39f58d9f5be8aa1d6473e41104e1e28af6243541
Jade Dominguez <plusjade@gmail.com>
2013-01-22 20:24:58 -0800

    Implement 'javascripts.load' block helper as the new interface to load javascripts.

--- lib/ruhoh/resources/javascripts/collection_view.rb
+++ lib/ruhoh/resources/javascripts/collection_view.rb
@@ -1,17 +1,28 @@
 module Ruhoh::Resources::Javascripts
   class CollectionView < Ruhoh::Resources::Base::CollectionView
-    def all
-      buffer = ''
-      master_layout = master.page_data['master_layout']
-      sub_layout = master.page_data['sub_layout']
-      scripts = @ruhoh.db.javascripts[master_layout] || []
-      scripts += @ruhoh.db.javascripts[sub_layout] || []
-      scripts += @ruhoh.db.javascripts["widgets"] || []
-      scripts.each do |script|
-        buffer += "<script src=\"#{script['url']}?#{rand()}\"></script>\n"
-      end
-    
-      buffer
+    # Load javascripts as defined within the given sub_context
+    #
+    # Example:
+    #   {{# javascripts.load }}
+    #     app.js
+    #     scroll.js
+    #   {{/ javascripts.load }}
+    #   (scripts are separated by newlines)
+    #
+    # This is a convenience method that will automatically create script tags
+    # with respect to ruhoh's internal URL generation mechanism; e.g. base_path.
+    #
+    # @returns[String] HTML script tags for given javascripts.
+    def load(sub_context)
+      collection = @ruhoh.resources.load_collection("javascripts")
+      javascripts = sub_context.split("\n").map{ |s| s.gsub(/\s/, '') }.delete_if(&:empty?)
+      javascripts.map { |name|
+        url = (name =~ /^(http:|https:)?\/\//i) ?
+          name :
+          (@ruhoh.to_url(collection.url_endpoint, name) + "?#{rand()}")
+        
+        "<script src='#{url}'></script>"
+      }.join("\n")
     end
   end
 end
