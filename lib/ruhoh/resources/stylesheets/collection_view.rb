01592950828af8516f4bc49cfcf042c1dd8b3d61
Jade Dominguez <plusjade@gmail.com>
2013-01-25 20:13:46 -0800

    Store and cache all calls to stylesheets.
    Exposing a writer to the cache allows us to mutate the cache in the event we need to rewrite the urls.
    
    This of course comes in handy when we do post-processing on assets and update the urls to include a digest hash.

--- lib/ruhoh/resources/stylesheets/collection_view.rb
+++ lib/ruhoh/resources/stylesheets/collection_view.rb
@@ -1,5 +1,13 @@
 module Ruhoh::Resources::Stylesheets
   class CollectionView < Ruhoh::Resources::Base::CollectionView
+    attr_accessor :_cache
+
+    def initialize(ruhoh)
+      super(ruhoh)
+      @collection = ruhoh.resources.load_collection("stylesheets")
+      @_cache = {}
+    end
+    
     # Load Stylesheets as defined within the given sub_context
     #
     # Example:
@@ -14,15 +22,25 @@ module Ruhoh::Resources::Stylesheets
     #
     # @returns[String] HTML link tags for given stylesheets
     def load(sub_context)
-      collection = @ruhoh.resources.load_collection("stylesheets")
       stylesheets = sub_context.split("\n").map{ |s| s.gsub(/\s/, '') }.delete_if(&:empty?)
       stylesheets.map { |name|
-        url = (name =~ /^(http:|https:)?\/\//i) ?
-          name :
-          (@ruhoh.to_url(collection.url_endpoint, name) + "?#{rand()}")
-        
-        "<link href='#{url}' type='text/css' rel='stylesheet' media='all'>"
+        "<link href='#{make_url(name)}' type='text/css' rel='stylesheet' media='all'>"
       }.join("\n")
+    end
+
+    protected
+
+    def make_url(name)
+      return name if name =~ /^(http:|https:)?\/\//i
+
+      path = if @_cache[name]
+        @_cache[name]
+      else
+        @_cache[name] = name
+        "#{name}?#{rand()}"
+      end
+
+      @ruhoh.to_url(@collection.url_endpoint, path)
     end
   end
 end
