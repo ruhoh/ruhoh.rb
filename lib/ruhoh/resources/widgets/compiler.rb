c43d85efaf0c97079b836a26aeb33018a8fd2bf2
Jade Dominguez <plusjade@gmail.com>
2013-01-28 19:50:53 -0800

    Add widget compiler.

---
+++ lib/ruhoh/resources/widgets/compiler.rb
@@ -0,0 +1,25 @@
+module Ruhoh::Resources::Widgets
+  class Compiler < Ruhoh::Resources::Base::Compiler
+    def run
+      collection = @collection
+      unless FileTest.directory? @collection.path
+        Ruhoh::Friend.say { yellow "#{collection.namespace.capitalize}: directory not found - skipping." }
+        return
+      end
+      Ruhoh::Friend.say { cyan "#{collection.namespace.capitalize}: (copying valid files)" }
+
+      compiled_path = Ruhoh::Utils.url_to_path(@ruhoh.to_url(@collection.url_endpoint), @ruhoh.paths.compiled)
+      FileUtils.mkdir_p compiled_path
+
+      files = @collection.files
+      # Don't copy over the layouts.
+      files.delete_if { |p| p['id'].end_with?('.html') }
+      files.each do |pointer|
+        compiled_file = File.join(compiled_path, pointer['id'])
+        FileUtils.mkdir_p File.dirname(compiled_file)
+        FileUtils.cp_r pointer['realpath'], compiled_file
+        Ruhoh::Friend.say { green "  > #{pointer['id']}" }
+      end
+    end
+  end
+end
