5c1f7cf34d1a5b7fd07c8899ccf6d92edf16ab38
Jade Dominguez <plusjade@gmail.com>
2013-01-31 22:19:28 -0800

    Use asset compiler for media.

--- lib/ruhoh/resources/media/compiler.rb
+++ lib/ruhoh/resources/media/compiler.rb
@@ -1,17 +1,4 @@
 module Ruhoh::Resources::Media
-  class Compiler < Ruhoh::Resources::Base::Compiler
-    def run
-      media_path = File.join(@ruhoh.paths.base, @collection.path)
-      unless FileTest.directory? media_path
-        Ruhoh::Friend.say { yellow "Media: directory not found - skipping." }
-        return
-      end
-      
-      Ruhoh::Friend.say { cyan "Media: (copying entire folder)" }
-      media = Ruhoh::Utils.url_to_path(@ruhoh.to_url(@collection.url_endpoint), @ruhoh.paths.compiled)
-      FileUtils.mkdir_p media
-      FileUtils.cp_r(File.join(media_path, '.'), media)
-      Ruhoh::Friend.say { green "  > done!" }
-    end
+  class Compiler < Ruhoh::Resources::Asset::Compiler
   end
 end
