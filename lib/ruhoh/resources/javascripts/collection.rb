464fa70ea31a5e4401bfadb855a7a45e26cfd5b5
Jade Dominguez <plusjade@gmail.com>
2013-02-10 16:56:00 -0800

    Remove unused widget methods.

--- lib/ruhoh/resources/javascripts/collection.rb
+++ lib/ruhoh/resources/javascripts/collection.rb
@@ -1,30 +1,7 @@
 module Ruhoh::Resources::Javascripts
   class Collection < Ruhoh::Resources::Base::Collection
-
     def url_endpoint
       "assets/#{namespace}"
     end
-
-    # widgets may register javascript dependencies, which are resolved here.
-    # Notes:
-    #   The automatic script inclusion is currently handled within the widget resource.
-    #   This differs from the auto-stylesheet inclusion relative to themes, 
-    #   which is handled in the stylesheet resource.
-    #   Make sure there are some standards with this.
-    def widget_javascripts
-      assets = []
-      @ruhoh.db.widgets.each_value do |widget|
-        next unless widget["javascripts"]
-        assets += Array(widget["javascripts"]).map {|path|
-          {
-            "url" => [@ruhoh.db.urls["widgets"], widget['name'], "javascripts", path].join('/'),
-            "id"  => File.join(@ruhoh.paths.widgets, widget['name'], "javascripts", path)
-          }
-        }
-      end
-      
-      assets
-    end
-    
   end
 end
--- lib/ruhoh/resources/stylesheets/collection.rb
+++ lib/ruhoh/resources/stylesheets/collection.rb
@@ -1,36 +1,7 @@
 module Ruhoh::Resources::Stylesheets
   class Collection < Ruhoh::Resources::Base::Collection
-
     def url_endpoint
       "assets/#{namespace}"
     end
-
-    # All styling is managed by the theme, including widget styles.
-    # If the theme provides widget stylesheets they will load automatically.
-    # theme.yml may also specify an explicit widget stylesheet to load.
-
-    # Create mappings for stylesheets registered to a given widget.
-    # A theme may provide widget stylesheets which will load automatically,
-    # provided they adhere to the default naming rules.
-    # Themes may also specify an explicit widget stylesheet to load.
-    # 
-    # Returns Array of asset objects.
-    def widget_stylesheets
-      assets = []
-      @ruhoh.db.widgets.each_key do |name|
-        default_name = "#{name}.css"
-        stylesheet = theme_config["widgets"][name] rescue default_name
-        stylesheet ||=  default_name
-        file = File.join(@ruhoh.paths.theme, "widgets", name, "stylesheets", stylesheet)
-        next unless File.exists?(file)
-        assets << {
-          "url" => [@ruhoh.db.urls["theme"], "widgets", name, "stylesheets", stylesheet].join('/'),
-          "id" => file
-        }
-      end
-
-      {"widgets" => assets}
-    end
-
   end
 end
