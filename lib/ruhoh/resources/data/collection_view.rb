6b2e3a527187c58751709d10c364c4f4320ebf3f
Jade Dominguez <plusjade@gmail.com>
2013-04-21 18:47:58 -0700

    Refactor data collectionView to be like everyone else.
    This is mainly to reduce complexity, I haven't really considered the performance implications.

--- lib/ruhoh/resources/data/collection_view.rb
+++ lib/ruhoh/resources/data/collection_view.rb
@@ -1,21 +1,23 @@
 module Ruhoh::Resources::Data
-  class CollectionView < OpenStruct
-    attr_accessor :collection
-    attr_accessor :master
-
-    extend Forwardable
-
-    def_instance_delegators :@collection, :dictionary, :find, :load_model_view
+  class CollectionView < Ruhoh::Base::CollectionView
 
     def initialize(collection)
-      @ruhoh = collection.ruhoh
-      @collection = collection
-      super(@collection.dictionary)
+      super(collection)
+
+      # Define direct access to the dictionary Hash object
+      # but don't overwrite methods if already defined.
+      dictionary.keys.each do |method|
+        (class << self; self; end).class_eval do
+          next if method_defined?(method)
+          define_method method do |*args, &block|
+            dictionary[method]
+          end
+        end
+      end
     end
 
     def [](attribute)
       __send__(attribute)
     end
-
   end
 end
