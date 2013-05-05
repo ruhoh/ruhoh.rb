0b3491f2e65e0949516659216e68702369b527dd
Jade Dominguez <plusjade@gmail.com>
2013-05-04 21:54:38 -0700

    #parse_page_file can be reused to parse layouts.
    Also ensure data.layout is always defined on layouts so the render layouts doesn't error.

--- lib/ruhoh/resources/layouts/model.rb
+++ lib/ruhoh/resources/layouts/model.rb
@@ -1,39 +1,16 @@
 module Ruhoh::Resources::Layouts
   class Model
-    include Ruhoh::Base::Modelable
+    include Ruhoh::Base::PageLike
 
     def process
-      parse_layout_file(@pointer['realpath'])
-    end
+      parsed_page = parse_page_file
+      # This ensures the call to @sub_layout.layout does not error.
+      parsed_page['data']['layout'] ||= nil
 
-    private
+      changed
+      notify_observers(parsed_page)
 
-    def parse_layout_file(*args)
-      path = File.__send__(:join, args)
-      raise "Layout file not found: #{path}" unless File.exist?(path)
-      data = {}
-      page = File.open(path, 'r:UTF-8') {|f| f.read }
-
-      front_matter = page.match(Ruhoh::Utils::FMregex)
-      if front_matter
-        data = YAML.load(front_matter[0].gsub(/---\n/, "")) || {}
-      end
-      
-      data['pointer'] = @pointer
-      data['id'] = @pointer['id']
-      
-      a = { 
-        "data" => data,
-        "content" => page.gsub(Ruhoh::Utils::FMregex, '')
-      }
-
-      @data = data
-      @content = a['content']
-
-      a
-    rescue Psych::SyntaxError => e
-      Ruhoh.log.error("ERROR in #{path}: #{e.message}")
-      nil
+      parsed_page
     end
   end
 end
