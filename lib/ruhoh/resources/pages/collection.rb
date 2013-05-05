a85e03a0d9bbc440cf7467f2d84183ed0a13e5f2
Jade Dominguez <plusjade@gmail.com>
2013-05-04 17:22:27 -0700

    Change routes_delete to accept the full pointer.

--- lib/ruhoh/resources/pages/collection.rb
+++ lib/ruhoh/resources/pages/collection.rb
@@ -8,11 +8,14 @@ module Ruhoh::Resources::Pages
     end
 
     def routes_add(route, pointer)
+      @routes ||= {}
       @routes[route] = pointer
     end
 
-    def routes_delete(route)
-      @routes.delete(route)
+    def routes_delete(pointer)
+      return unless @routes
+      route = @routes.find{ |k, v| v == pointer }
+      @routes.delete(route[0]) if route
     end
   end
 
