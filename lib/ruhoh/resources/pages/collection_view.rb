42b431624bf5b278e4050e0beeb1f60e70fe1235
Jade Dominguez <plusjade@gmail.com>
2013-05-18 16:09:53 -0700

    Move paginator methods to module.

--- lib/ruhoh/resources/pages/collection_view.rb
+++ lib/ruhoh/resources/pages/collection_view.rb
@@ -1,10 +1,12 @@
 require 'ruhoh/views/helpers/categories'
 require 'ruhoh/views/helpers/tags'
+require 'ruhoh/views/helpers/paginator'
 module Ruhoh::Resources::Pages
 
   class CollectionView < SimpleDelegator
     include Ruhoh::Views::Helpers::Tags
     include Ruhoh::Views::Helpers::Categories
+    include Ruhoh::Views::Helpers::Paginator
 
     def all
       dictionary.each_value.find_all { |model|
@@ -22,42 +24,6 @@ module Ruhoh::Resources::Pages
       latest = config['latest']
       latest ||= 10
       (latest.to_i > 0) ? all[0, latest.to_i] : all
-    end
-
-    # current_page is set via a compiler or previewer
-    # in which it can discern what current_page to serve
-    def paginator
-      per_page = config["paginator"]["per_page"] rescue 5
-      current_page = master.page_data['current_page'].to_i
-      current_page = current_page.zero? ? 1 : current_page
-      offset = (current_page-1)*per_page
-
-      page_batch = all[offset, per_page]
-      raise "Page does not exist" unless page_batch
-      page_batch
-    end
-
-    def paginator_navigation
-      paginator_config = config["paginator"] || {}
-      page_count = all.length
-      total_pages = (page_count.to_f/paginator_config["per_page"]).ceil
-      current_page = master.page_data['current_page'].to_i
-      current_page = current_page.zero? ? 1 : current_page
-  
-      pages = total_pages.times.map { |i| 
-        url = if i.zero? && paginator_config["root_page"]
-          paginator_config["root_page"]
-        else
-          "#{paginator_config["namespace"]}/#{i+1}"
-        end
-        
-        {
-          "url" => ruhoh.to_url(url),
-          "name" => "#{i+1}",
-          "is_active_page" => (i+1 == current_page)
-        }
-      }
-      pages 
     end
 
     # Internal: Create a collated pages data structure.
---
+++ lib/ruhoh/views/helpers/paginator.rb
@@ -0,0 +1,39 @@
+module Ruhoh::Views::Helpers
+  module Paginator
+    # current_page is set via a compiler or previewer
+    # in which it can discern what current_page to serve
+    def paginator
+      per_page = config["paginator"]["per_page"] rescue 5
+      current_page = master.page_data['current_page'].to_i
+      current_page = current_page.zero? ? 1 : current_page
+      offset = (current_page-1)*per_page
+
+      page_batch = all[offset, per_page]
+      raise "Page does not exist" unless page_batch
+      page_batch
+    end
+
+    def paginator_navigation
+      paginator_config = config["paginator"] || {}
+      page_count = all.length
+      total_pages = (page_count.to_f/paginator_config["per_page"]).ceil
+      current_page = master.page_data['current_page'].to_i
+      current_page = current_page.zero? ? 1 : current_page
+
+      pages = total_pages.times.map { |i| 
+        url = if i.zero? && paginator_config["root_page"]
+          paginator_config["root_page"]
+        else
+          "#{paginator_config["namespace"]}/#{i+1}"
+        end
+
+        {
+          "url" => ruhoh.to_url(url),
+          "name" => "#{i+1}",
+          "is_active_page" => (i+1 == current_page)
+        }
+      }
+      pages 
+    end
+  end
+end
