c9488a0f965f2b5d71575f479469857517f1bee5
Casper Von B <casperbeyer@gmail.com>
2013-05-23 20:56:36 +0800

    Made widgets compiler respect model excludes.

--- lib/ruhoh/resources/widgets/compiler.rb
+++ lib/ruhoh/resources/widgets/compiler.rb
@@ -13,15 +13,31 @@ module Ruhoh::Resources::Widgets
       compiled_path = Ruhoh::Utils.url_to_path(@ruhoh.to_url(@collection.url_endpoint), @ruhoh.paths.compiled)
       FileUtils.mkdir_p compiled_path
 
-      files = @collection.files.values
-      # Don't copy over the layouts.
-      files.delete_if { |p| p['id'].end_with?('.html') }
+      files = collection.files.values
+      files.delete_if { |p| !is_valid_file? (p['id']) }
+
       files.each do |pointer|
         compiled_file = File.join(compiled_path, pointer['id'])
         FileUtils.mkdir_p File.dirname(compiled_file)
         FileUtils.cp_r pointer['realpath'], compiled_file
         Ruhoh::Friend.say { green "  > #{pointer['id']}" }
       end
+    end
+
+    def is_valid_file?(filepath)
+      return false if filepath.end_with?('.html')
+
+      collection.widgets.each do |name|
+        widget_config = collection.config[name] || {}
+
+        model = collection.find("#{ name }/#{ (widget_config['use'] || "default") }")
+        next unless model
+
+        excludes = Array(model.data['exclude']).map { |node| Regexp.new(node) }
+        excludes.each { |regex| return false if filepath =~ regex }
+      end
+
+      true
     end
   end
 end
