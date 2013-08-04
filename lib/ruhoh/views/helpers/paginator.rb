bb13a5ab47e69abd338569ff8a952b34f3b826b0
Alexei Matyushkin <am@mudasobwa.ru>
2013-08-04 07:41:55 +0400

    Paginator is now intelligent.

--- lib/ruhoh/views/helpers/paginator.rb
+++ lib/ruhoh/views/helpers/paginator.rb
@@ -13,6 +13,7 @@ module Ruhoh::Views::Helpers
       page_batch
     end
 
+    # [1][2] ... [n-1][n][n+1] ... [last-1][last]
     def paginator_navigation
       paginator_config = config["paginator"] || {}
       page_count = all.length
@@ -20,17 +21,42 @@ module Ruhoh::Views::Helpers
       current_page = master.page_data['current_page'].to_i
       current_page = current_page.zero? ? 1 : current_page
 
-      pages = total_pages.times.map { |i| 
+#      pages = total_pages.times.map { |i| 
+#        url = if i.zero? && paginator_config["root_page"]
+#                paginator_config["root_page"]
+#              else
+#                "#{paginator_config["url"]}/#{i+1}"
+#              end
+#
+#        {
+#          "url" => ruhoh.to_url(url),
+#          "name" => "#{i+1}",
+#          "is_active_page" => (i+1 == current_page)
+#        }
+#     }
+      left_dots = ((current_page+1)/2).ceil
+      right_dots = ((total_pages+current_page)/2).ceil
+      borders = paginator_config["borders"]
+
+      pages = total_pages.times.select { |i|
+        i+1 <= borders || i+1 > total_pages-borders || 
+        (i+1 >= current_page-(borders/2).ceil && i+1 <= current_page+(borders/2).ceil) || 
+        i+1 == left_dots || i+1 == right_dots                    
+      }.map { |i|
+        ii=i+1
         url = if i.zero? && paginator_config["root_page"]
                 paginator_config["root_page"]
               else
-                "#{paginator_config["url"]}/#{i+1}"
+                "#{paginator_config["url"]}/#{ii}"
               end
-
+        name = (i+1 > borders) && (i+1 < total_pages-borders) && 
+               ((i+1 < current_page-(borders/2).ceil) || (i+1 > current_page+(borders/2).ceil)) && 
+               (i+1 == left_dots || i+1 == right_dots) ? '…' : "#{i+1}"
         {
           "url" => ruhoh.to_url(url),
-          "name" => "#{i+1}",
-          "is_active_page" => (i+1 == current_page)
+          "name" => name,
+          "is_active_page" => (i+1 == current_page),
+          "title" => "#{i+1}"
         }
       }
       pages 
