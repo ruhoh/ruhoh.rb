0554ff60a65bfbb79d3598db7df0aa957d03bde6
Jade Dominguez <plusjade@gmail.com>
2013-09-09 23:35:45 -0700

    Data.yml should be discoverable on every cascade level.

--- lib/ruhoh/resources/data/collection.rb
+++ lib/ruhoh/resources/data/collection.rb
@@ -2,8 +2,24 @@ module Ruhoh::Resources::Data
   class Collection
     include Ruhoh::Base::Collectable
 
+    # TODO: This is ugly but it works. 
+    # Should handle data extensions in the cascade more elegantly
     def dictionary
-      Ruhoh::Parse.data_file(@ruhoh.paths.base, resource_name) || {}
+      found_path_prefix = nil
+
+      @ruhoh.cascade.reverse.map do |h|
+        path_prefix = File.join(h["path"], resource_name)
+
+        ["#{ path_prefix }.json", "#{ path_prefix }.yml", "#{ path_prefix }.yaml"].each do |file|
+          found_path_prefix = path_prefix and break if File.exist?(file)
+        end
+
+        break if found_path_prefix
+      end
+
+      return {} unless found_path_prefix
+
+      Ruhoh::Parse.data_file(found_path_prefix) || {}
     end
   end
 end
