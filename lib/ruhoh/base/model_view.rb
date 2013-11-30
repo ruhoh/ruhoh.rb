c920e8b0de386c8f23d14ce3a7ce3a85b122b1ed
Alexei Matyushkin <am@mudasobwa.ru>
2013-11-30 11:58:04 +0400

    FIXED: Non-ASCII symbols were translated to HTML entities within ModelView.

--- lib/ruhoh/base/model_view.rb
+++ lib/ruhoh/base/model_view.rb
@@ -106,7 +106,7 @@ module Ruhoh::Base
 
       # Return a summary element if specified
       summary_el = content_doc.at_css('.summary')
-      return summary_el.to_html unless summary_el.nil?
+      return summary_el.to_html(:encoding => 'UTF-8') unless summary_el.nil?
 
       # Get the configuration parameters
       # Default to the parameters provided in the page itself
@@ -154,7 +154,7 @@ module Ruhoh::Base
         summary_doc << node
       end
 
-      summary_doc.to_html
+      summary_doc.to_html :encoding => 'UTF-8'
     end
 
     def next
