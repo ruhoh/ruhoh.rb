c84947025a1236875b7fb2bfa6b443e5975dd960
Andreas Richter <andy@richteralsi.com>
2013-12-09 21:21:39 -0500

    Missing change.

--- lib/ruhoh/resources/javascripts/model_view.rb
+++ lib/ruhoh/resources/javascripts/model_view.rb
@@ -1,7 +1,15 @@
 module Ruhoh::Resources::Javascripts
   class ModelView < SimpleDelegator
     def url()
-      self.collection.make_url(self.pointer['id'])
+      	self.collection.make_url(self.pointer['id'])
+    end
+
+    def id()
+    	self.pointer['id']
+    end
+
+    def path()
+    	self.pointer['realpath']
     end
   end
 end
